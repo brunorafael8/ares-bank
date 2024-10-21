06999119b98a8ac0a9d4eabc950cfd8f709fbc89
brunorafael8 <bruninho_lito@hotmail.com>
2024-10-21 14:50:05 -0300

    feat: create authentication page on pages

---
+++ apps/web/src/pages/Authentication/index.tsx
@@ -0,0 +1,106 @@
+import { Button, Input, Label } from "@ui/components";
+import { SyntheticEvent, useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function AuthenticationPage() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  async function onSubmit(event: SyntheticEvent) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }
+
+  return (
+    <>
+      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+          <div className="absolute inset-0 bg-zinc-900" />
+          <div className="relative z-20 flex items-center text-lg font-medium">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="mr-2 h-6 w-6"
+            >
+              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+            </svg>
+            Ares Bank
+          </div>
+          <div className="relative z-20 mt-auto">
+            <blockquote className="space-y-2">
+              <p className="text-lg">
+                The future of banking, today. Ares Bank is a fully digital
+                banking experience designed for the modern world. We offer a
+                seamless mobile-first approach, cutting-edge security, and
+                personalized financial tools to empower you to take control of
+                your finances.
+              </p>
+              <footer className="text-sm">Sofia Davis</footer>
+            </blockquote>
+          </div>
+        </div>
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                Sign in or create an account
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                Enter your email below.
+              </p>
+            </div>
+            <div className="first-letter:grid gap-6">
+              <form onSubmit={onSubmit}>
+                <div className="grid gap-2">
+                  <div className="grid gap-1">
+                    <Label className="sr-only" htmlFor="email">
+                      Email
+                    </Label>
+                    <Input
+                      id="email"
+                      placeholder="name@example.com"
+                      type="email"
+                      autoCapitalize="none"
+                      autoComplete="email"
+                      autoCorrect="off"
+                      disabled={isLoading}
+                    />
+                  </div>
+                  <Button disabled={isLoading}>
+                    {isLoading && <div>Loading...</div>}
+                    Continue with email
+                  </Button>
+                </div>
+              </form>
+            </div>
+            <p className="px-8 text-center text-sm text-muted-foreground">
+              By clicking continue, you agree to our{" "}
+              <Link
+                to="/terms"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Terms of Service
+              </Link>{" "}
+              and{" "}
+              <Link
+                to="/privacy"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Privacy Policy
+              </Link>
+              .
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
